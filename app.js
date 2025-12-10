3ce131ded708d02a12f33d0a5d4c089a1e7801c3
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-12-10 22:56:20 +0800

    Set up requirements to start session cookies

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express");
 const path = require("node:path");
+
+const express = require("express");
 const router = require("./routes/userRoute")
 
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(passport.session());
 
 const PORT = 3000;
 
